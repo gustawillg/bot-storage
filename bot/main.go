73b1b1681dc18ad5d0f1a3afcb2355d6c77cfb11
Gustavo <coartem2@proton.me>
2024-02-23 14:07:59 -0300

    Update main.go

--- bot/main.go
+++ bot/main.go
@@ -19,10 +19,7 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Panic(err)
-	}
+	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
