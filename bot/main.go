8a69d8631b5e55760f56ae5e8ad48ecca7697290
Gustavo <coartem2@proton.me>
2024-02-23 13:48:39 -0300

    start

---
+++ bot/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("7038585765:AAEAhlGInnGdG59xHffGubPyR8HN07lotoo")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Autorizado como %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gustawillg/bot-storage
+
+go 1.21.6
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
