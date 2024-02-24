5fc67e2abe665a5d6a7a345f92b15958347c16e7
Gustavo <coartem2@proton.me>
2024-02-24 17:28:39 -0300

    teste

--- bot/main.go
+++ bot/main.go
@@ -1,51 +1,72 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"os"
 
-	"bazil.org/fuse"
-	"bazil.org/fuse/fs"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-// Implementação do FileSystem
-type FS struct{}
-
-// Implementação do método Root
-func (f FS) Root() (fs.Node, error) {
-	return Dir{}, nil
-}
-
-// Implementação do Directory
-type Dir struct{}
-
-// Implementação dos métodos do Diretório
-func (d Dir) Attr() fuse.Attr {
-	return fuse.Attr{Inode: 1, Mode: os.ModeDir | 0755}
-}
-
 func main() {
-	flag.Parse()
-
-	// Montar o FileSystem
-	c, err := fuse.Mount(
-		os.Args[1],
-	)
+	bot, err := tgbotapi.NewBotAPI("7038585765:AAEAhlGInnGdG59xHffGubPyR8HN07lotoo")
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer c.Close()
-
-	// Servir o FileSystem
-	err = fs.Serve(c, FS{})
-	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
-	// Esperar até que seja desmontado
-	<-c.Ready
-	if err := c.MountError; err != nil {
-		log.Fatal(err)
+	bot.Debug = true
+
+	log.Printf("Autorizado como %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// Handle document messages
+		handleDocument(bot, update)
+	}
+}
+
+func handleDocument(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	if update.Message.Document == nil {
+		return
+	}
+
+	fileID := update.Message.Document.FileID
+	fileConfig := tgbotapi.FileConfig{
+		FileID: fileID,
+	}
+
+	file, err := bot.GetFile(fileConfig)
+	if err != nil {
+		log.Println("Erro ao obter o arquivo:", err)
+		return
+	}
+
+	fileURL := file.Link(bot.Token)
+
+	// Aqui você pode baixar o arquivo e armazená-lo onde desejar
+	log.Println("URL do arquivo:", fileURL)
+
+	// Exemplo de envio de vídeo de volta
+	sendVideo(bot, update.Message.Chat.ID, fileURL)
+}
+
+func sendVideo(bot *tgbotapi.BotAPI, chatID int64, videoURL string) {
+	video := tgbotapi.NewVideoUpload(chatID, videoURL)
+	video.Caption = "Seu vídeo"
+
+	_, err := bot.Send(video)
+	if err != nil {
+		log.Println("Erro ao enviar vídeo:", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.21.6
 
 require bazil.org/fuse v0.0.0-20230120002735-62a210ff1fd5
 
-require golang.org/x/sys v0.17.0 // indirect
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	golang.org/x/sys v0.17.0 // indirect
+)
