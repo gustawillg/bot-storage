7153108837303c577a2d0c095b41426b32db8619
Gustavo <coartem2@proton.me>
2024-02-29 12:26:35 -0300

    oh ow

--- bot/main.go
+++ bot/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"math"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -78,4 +82,62 @@ func sendVideo(bot *tgbotapi.BotAPI, chatID int64, videoURL string) {
 	if err != nil {
 		log.Println("Erro ao enviar vídeo:", err)
 	}
+}
+
+// Função para enviar chunks de um vídeo para o Telegram
+func sendVideoChunks(bot *tgbotapi.BotAPI, chatID int64, videoFilePath string) error {
+	// Abra o arquivo de vídeo
+	videoFile, err := os.Open(videoFilePath)
+	if err != nil {
+		return err
+	}
+	defer videoFile.Close()
+
+	// Determine o tamanho do arquivo
+	fileInfo, err := videoFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileInfo.Size()
+
+	// Defina o tamanho máximo do chunk (por exemplo, 50 MB)
+	chunkSize := int64(50 * 1024 * 1024) // 50 MB
+	numChunks := int(math.Ceil(float64(fileSize) / float64(chunkSize)))
+
+	// Envie cada chunk como um documento separado
+	for i := 0; i < numChunks; i++ {
+		// Calcule os offsets para cada chunk
+		offset := int64(i) * chunkSize
+		chunkBytes := min(chunkSize, fileSize-offset)
+
+		// Leia os bytes do chunk
+		chunkBuffer := make([]byte, chunkBytes)
+		_, err := videoFile.ReadAt(chunkBuffer, offset)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		// Envie o chunk como um documento para o Telegram
+		videoPart := tgbotapi.FileBytes{
+			Name:  fmt.Sprintf("part_%d.mp4", i+1),
+			Bytes: chunkBuffer,
+		}
+
+		videoConfig := tgbotapi.NewDocumentUpload(chatID, videoPart)
+		_, err = bot.Send(videoConfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Função auxiliar para encontrar o mínimo entre dois inteiros
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,6 @@ module github.com/gustawillg/bot-storage
 
 go 1.21.6
 
-require bazil.org/fuse v0.0.0-20230120002735-62a210ff1fd5
-
 require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	golang.org/x/sys v0.17.0 // indirect
-)
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
