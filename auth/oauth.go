6f38616c6959f587fa86c74d0b89c582807fc004
Gustavo <coartem2@proton.me>
2024-03-02 12:41:28 -0300

    oauth

---
+++ auth/oauth.go
@@ -0,0 +1,62 @@
+package oauth
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	oauthConfig *oauth2.Config
+)
+
+const (
+	oauthGoogleURLAPI = "https://www.googleapis.com/oauth2/v2/userinfo"
+)
+
+func init() {
+	oauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     "481585171843-qlhj01c6hhqtjaoqpbrnj8satngah5vk.apps.googleusercontent.com",
+		ClientSecret: "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",
+		Scopes:       []string{"https://www.googleapis.com/auth/drive"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/login", handleGoogleLogin)
+	http.HandleFunc("/callback", handleGoogleCallback)
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html><body><a href="/login">Google Log In</a></body></html>`
+	fmt.Fprintf(w, htmlIndex)
+}
+
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := oauthConfig.AuthCodeURL("")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	token, err := oauthConfig.Exchange(r.Context(), code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := oauthConfig.Client(r.Context(), token)
+	response, err := client.Get(oauthGoogleURLAPI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	var htmlSuccess = `<html><body>Authentication Successful!</body></html>`
+	fmt.Fprintf(w, htmlSuccess)
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,17 @@ module github.com/gustawillg/bot-storage
 
 go 1.21.6
 
-require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+require (
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
 
-require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	golang.org/x/oauth2 v0.17.0
+)
